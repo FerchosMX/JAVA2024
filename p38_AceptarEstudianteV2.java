6e45e420855758cb2a557913d5bf8315cb136d37
FerchosMX <fernandodlt02@gmail.com>
2024-08-28 10:42:13 -0600

    COMMIT

--- p38_AceptarEstudianteV2.java
+++ p38_AceptarEstudianteV2.java
@@ -10,34 +10,32 @@ public class p38_AceptarEstudianteV2 {
         
         String nombre;
         char sexo;
-        int edad,cal1,cal2,cal3,prom;
+        int edad,cal1,cal2,cal3;
+        double prom;
 
 
         System.out.println("--- Universidad Chilaquiles ---");
-        System.out.print("      Introduzca su nombre: ");
+        System.out.print("    Introduzca su nombre: ");
         nombre = scanner.nextLine();
-        System.out.print("      Introduzca su sexo (M/F): ");
-        sexo = scanner.next();
-        System.out.print("      Introduzca su edad: ");
+        System.out.print("    Introduzca su sexo M/F: ");
+        sexo = scanner.next().charAt(0);
+        System.out.print("    Introduzca su edad: ");
         edad = scanner.nextInt();
         System.out.println("\n  Ahora introduzca sus últimas 3 calificaciones");
-        System.out.println("1ra calificación: ");
+        System.out.print("    1ra calificación: ");
         cal1 = scanner.nextInt();
-        System.out.println("2da calificación: ");
+        System.out.print("    2da calificación: ");
         cal2 = scanner.nextInt();
-        System.out.println("3ra calificación: ");
+        System.out.print("    3ra calificación: ");
         cal3 = scanner.nextInt();
 
-        prom = (cal1+cal2+cal3)/3;
+        prom = (cal1+cal2+cal3)/3.0;
 
-        if (sexo = 'M') {
-            System.out.println("Lo lamento pero no estás aceptado");
-            } else if (edad<21) {
-                System.out.println("Lo lamento pero no estás aceptado");
-            } else if (prom<8) {
-                System.out.println("Lo lamento pero no estás aceptado");
-            }
+        if (sexo=='F' && edad>=21 && prom>8 && prom<=9.5) {
+            System.out.println("YAY! Bienvenido a universidad chilaquiles");
+        } else {
+            System.out.println("Lo lamento pero no te aceptamos");
         }
     }
-    
 }
+
