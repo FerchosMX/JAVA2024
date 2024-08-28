a724325b3780a76e8a4307611238309b0497ef49
FerchosMX <fernandodlt02@gmail.com>
2024-08-28 10:59:45 -0600

    commit

--- p33_NumerosConsecutivos.java
+++ p33_NumerosConsecutivos.java
@@ -8,11 +8,15 @@ public class p33_NumerosConsecutivos {
         Scanner obj = new Scanner(System.in);
         System.out.print("\033[H\033[2J"); System.out.flush();
 
-        if (a) {
-            System.out.println("");
-        } else {
-            
+        System.out.println("Dame 3 numeros: "); 
+        a=obj.nextInt();
+        b=obj.nextInt();
+        c=obj.nextInt();
+        if (a+1==b && b+1==c) {
+                System.out.println("Si son números consecutivos: "+a+","+b+","+c);
+        } else System.out.println("No son números consecutivos");
+ 
         }
     }
     
-}
+
