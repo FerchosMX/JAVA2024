d4da5a9236b64ea00445070c37234c453b30ec19
FerchosMX <fernandodlt02@gmail.com>
2024-09-09 12:33:07 -0600

    Práctica Examen

--- p61_SistemaPapelería.java
+++ p61_SistemaPapelería.java
@@ -1,3 +1,5 @@
+// FERNANDO DE LA TORRE LÓPEZ DE LARA - PRÁCTICA 1ER EXAMEN POO
+
 /*
 Objetivo - Una empresa fotocopiadora requiere un programa que le permita llevar el control de su venta de
 fotocopias. El valor de la copia es: 
@@ -16,14 +18,17 @@ public class p61_SistemaPapelería {
 
         Scanner obj = new Scanner(System.in);
 
+        // Declaramos Variables
         int CostoCarta=3, CostoOficio=4, CostoDoficio=6, precioFinal=0, Cantidad=0, CantidadC=0,CantidadO=0,CantidadDO=0,ContadorVentas=0;
         char tipoCopia, resp;
+
 
         System.out.println("--------------------------------");
         System.out.println("Papelería la Malena, SA. de CV.");
         System.out.println("Sistema de ventas de copias");
         System.out.println("--------------------------------\n");
 
+        // Hacemos el sistema de elección de copias y asignación de variables
         do {
             System.out.print("\nTipo de copia: (C)arta $3, (O)ficio $4, (D)oble Oficio $6 -> ");
             tipoCopia = obj.next().charAt(0);
@@ -31,10 +36,11 @@ public class p61_SistemaPapelería {
             System.out.print("Cantidad de copias: ");
             Cantidad = obj.nextInt();
 
+            // Comenzamos los switch statements para cada una de las opciones de venta 
             switch (Character.toUpperCase(tipoCopia)) {
                 case 'C':
-                    CantidadC = Cantidad;
-                    precioFinal += Cantidad * CostoCarta;
+                    CantidadC = Cantidad; // Asignación de variables
+                    precioFinal += Cantidad * CostoCarta; 
                     break;
                 case 'O':
                     CantidadO = Cantidad;
@@ -50,13 +56,15 @@ public class p61_SistemaPapelería {
                     break;
 
                 }
-                
+
+                // Aquí se va sumando la cantidad de ventas que quiere el usuario
                 ContadorVentas++;
                 System.out.print("Otra venta (S/N)? \n"); 
                 resp=Character.toUpperCase(obj.next().charAt(0));        
           
             } while (resp!= 'N');
 
+        // Diseño visual e impresión de resultados
         System.out.println("\nProceso Terminado\n");
 
         System.out.println("--------------------------------");
@@ -71,6 +79,7 @@ public class p61_SistemaPapelería {
         System.out.println("-----------------------");
         System.out.print("Tot. de ventas: "+ContadorVentas+" - $ "+precioFinal);
 
+        // If statement para clasificar el precio total en uno de los 3 tipos de ventas
         if (precioFinal<=50) {
             System.out.println("\nEsta venta es una: Venta Moderada");
         } else if (precioFinal<=100 && precioFinal>=50) {
